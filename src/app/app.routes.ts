cbcf96ed53014503b538f7dfc1c100bcbd63030e
atasdann <abdulkadir.tasdann@gmail.com>
2024-02-18 02:40:05 +0300

    I practiced on Angular standalone structure

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { PostComponent } from '../components/post/post.component';
+import { CommentComponent } from '../components/post/comment/comment.component';
+import { NotFoundComponent } from '../components/error_pages/not-found/not-found.component';
+
+export const routes: Routes = [
+  {path:'',component:Page1Component},
+  {path:'page1',component:Page1Component},
+  {path:'page2',component:Page2Component},
+  {path:'posts',children:[
+    {path:'view', component:PostComponent},
+    {path:'comments/:id',component:CommentComponent}
+  ]},
+  {path:"**",component:NotFoundComponent}
+];
